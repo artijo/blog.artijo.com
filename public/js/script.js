e5c5e00b31992450fa79b6c696a3c8106a8dcbc1
tijo <81065469+artijo@users.noreply.github.com>
2024-05-15 10:58:13 +0700

    chore: Update script.js and Footer.astro to improve footer context element

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,1 @@
-const contextElement = document.querySelector("#ft-context");
-if (contextElement) {
-  contextElement.innerHTML = `&copy;${new Date().getFullYear()} <a href='https://artijo.com/' target='_blank'>artijo.com </a>ALL RIGHTS RESERVED.`;
-}
+// add script not optimized

e1d7cb6521d9a4aa9b2661ef4cd971c31e040b03
tijo <81065469+artijo@users.noreply.github.com>
2024-05-15 10:44:21 +0700

    chore: Update instant.js, Footer.astro, Mode.astro, and Layout.astro

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+const contextElement = document.querySelector("#ft-context");
+if (contextElement) {
+  contextElement.innerHTML = `&copy;${new Date().getFullYear()} <a href='https://artijo.com/' target='_blank'>artijo.com </a>ALL RIGHTS RESERVED.`;
+}
---
+++ public/js/swMode.js
@@ -0,0 +1,20 @@
+const ToggleMode = () => {
+    const element = document.documentElement;
+    element.classList.toggle('dark');
+    const isDark = element.classList.contains('dark');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  };
+
+  (document.getElementById('mode')).addEventListener('click', ToggleMode);
+  document.addEventListener(
+  'click',
+  function (e) {
+    if (e.target.closest('#navtoggle')) {
+      document.body.classList.toggle('nav-open');
+    }
+    if ((e.target).closest('nav a')) {
+      document.body.classList.remove('nav-open');
+    }
+  },
+  false
+);

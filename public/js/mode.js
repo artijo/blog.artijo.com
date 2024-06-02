760a670f09a90c2d4b5d5e3c39bf00a83bb9caf9
tijo <81065469+artijo@users.noreply.github.com>
2024-06-02 07:32:33 +0700

    chore: Update swMode.js to set theme color based on user preference

--- public/js/mode.js
+++ public/js/mode.js
@@ -4,6 +4,8 @@ if (
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
   document.documentElement.classList.add("dark");
+  document.querySelector('meta[name="theme-color"]').setAttribute("content", "#020617");
 } else {
   document.documentElement.classList.remove("dark");
+  document.querySelector('meta[name="theme-color"]').setAttribute("content", "#F1F5F9");
 }
